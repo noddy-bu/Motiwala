82d742ce3dc93e75bf24025e5efd0d8b28cb23a5
Faisal <khanfasial.makent@gmail.com>
2024-05-14 20:08:07 +0530

    error solve

--- app/Http/Controllers/common/EsignAadharController.php
+++ app/Http/Controllers/common/EsignAadharController.php
@@ -134,7 +134,7 @@ class EsignAadharController extends Controller
         $generate = $this->generate_pdf($client_id);
 
 
-        $upload_pdf = $this->upload_pdf($signature, $date, $credentials, $upload_key, $policy, $algorithm,$client_id,  $url);
+        $upload_pdf = $this->upload_pdf($signature, $date, $credentials, $upload_key, $policy, $algorithm, $client_id,  $url);
 
         if($upload_pdf !== ""){
 
@@ -233,7 +233,7 @@ class EsignAadharController extends Controller
 
     function upload_pdf($signature, $date, $credentials, $upload_key, $policy, $algorithm, $client_id, $url){
 
-        $file_path = public_path('storage/generate_pdf/generated_pdf_'.$client_id.'.pdf');
+        $file_path = url('storage/generate_pdf/generated_pdf_'.$client_id.'.pdf');
 
         $curl = curl_init();
 
