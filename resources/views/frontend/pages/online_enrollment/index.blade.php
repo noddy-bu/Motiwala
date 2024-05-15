8dbd1988566462a35382d3c1cc2e4a5a3eba3a20
Faisal <khanfasial.makent@gmail.com>
2024-05-15 10:52:44 +0530

    loading screen

--- resources/views/frontend/pages/online_enrollment/index.blade.php
+++ resources/views/frontend/pages/online_enrollment/index.blade.php
@@ -211,7 +211,7 @@
                 event.preventDefault();
 
                 var button = $(form).find('button[type="submit"]').html();
-                $(form).find('button[type="submit"]').html('<i class="las la-spinner la-spin"></i>');
+                $(form).find('button[type="submit"]').html('please wait... <i class="las la-spinner la-spin"></i>');
                 $(form).find('button[type="submit"]').css('pointer-events', 'none');
                 
                 $.ajax({
@@ -411,7 +411,7 @@
             event.preventDefault();
 
             var button = $(form).find('button[type="submit"]').html();
-            $(form).find('button[type="submit"]').html('<i class="las la-spinner la-spin"></i>');
+            $(form).find('button[type="submit"]').html('please wait... <i class="las la-spinner la-spin"></i>');
             $(form).find('button[type="submit"]').css('pointer-events', 'none');
             
             $.ajax({
@@ -474,7 +474,7 @@
                 event.preventDefault();
 
                 var button = $(form).find('button[type="submit"]').html();
-                $(form).find('button[type="submit"]').html('<i class="las la-spinner la-spin"></i>');
+                $(form).find('button[type="submit"]').html('please wait... <i class="las la-spinner la-spin"></i>');
                 $(form).find('button[type="submit"]').css('pointer-events', 'none');
                 
                 $.ajax({
