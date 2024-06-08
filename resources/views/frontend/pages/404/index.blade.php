3b5495123f0fad191cd0a80468c7b3b536246920
SkZubeirAli <zuber.nexgeno@gmail.com>
2024-06-08 17:17:27 +0530

    hh

--- resources/views/frontend/pages/404/index.blade.php
+++ resources/views/frontend/pages/404/index.blade.php
@@ -18,7 +18,7 @@
 
 <section class="404">
     <div class="container">
-        <div class="row">
+        <div class="row"> 
             <div class="col-md-12">
                 <div class="container_404 d-flex align-items-center justify-content-center flex-column pb-md-5">
                     <h1>404</h1>
