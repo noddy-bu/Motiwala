9cfc85d8795fbb341651e2e18e219912f0c04cc5
SkZubeirAli <zuber.nexgeno@gmail.com>
2024-06-20 18:33:48 +0530

    change

--- resources/views/frontend/partials/footer2.blade.php
+++ resources/views/frontend/partials/footer2.blade.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row gap-md-0 gap-2">
                 <div class="col-md-5">
-                    <ul>
+                    <ul class="d-flex justify-content-center">
                         <li class="ms-md-1 ms-0">
                             <a href="terms-conditions">  Terms Conditions</a>
                         </li>
@@ -23,10 +23,33 @@
                         </li> -->
                     </ul>
                 </div>
-                <div class="col-md-4 copyright_text">
+
+                <div class="col-md-3 d-md-none d-block">
+                    <ul class="float_right d-flex justify-content-center">
+                        <li>
+                            <a target="_blank" href="https://api.whatsapp.com/send?phone=+919920077780&text=Hi%2C+I+am+contacting+you+through+your+website+from+desktop+view+https%3A%2F%2Fhttps://motiwalajewels.in/%2F">
+                                <i class="lab la-whatsapp"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="https://www.facebook.com/MOTIWALA.JEWELS">
+                                <i class="lab la-facebook-f"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="lab la-instagram"></i>
+                            </a>
+                        </li>
+                       
+                        <li>Let's Get Together</li>
+                    </ul>
+                </div>
+                
+                <div class="col-md-4 copyright_text d-flex justify-content-center">
                     <span class="text-center">Copyright © 2024 Under Motiwala & Sons</span>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-3 d-md-block d-none">
                     <ul class="float_right">
                         <li>
                             <a target="_blank" href="https://api.whatsapp.com/send?phone=+919920077780&text=Hi%2C+I+am+contacting+you+through+your+website+from+desktop+view+https%3A%2F%2Fhttps://motiwalajewels.in/%2F">
