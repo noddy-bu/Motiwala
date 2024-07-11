f969d1c3ab4238874a8ef65fc9a346b4791a1b95
Mahtab Alam <mehtab.makent@gmail.com>
2024-07-11 10:01:55 +0530

    whatsapp link change

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -109,7 +109,7 @@
         <div class="col-md-6 text-md-end text-center footer_social">
           <ul class="float_right">
             <li>
-              <a target="_blank" href="https://api.whatsapp.com/send?phone=+919920077780&text=Hi%2C+I+am+contacting+you+through+your+website+from+desktop+view+https%3A%2F%2Fhttps://motiwalajewels.in/%2F">
+              <a target="_blank" href="https://api.whatsapp.com/send?phone=+919920077780&text=Hi%2C+I+am+contacting+you+through+your+website+from+desktop+view+https://motiwalajewels.in/%2F">
                 <i class="lab la-whatsapp"></i>
               </a>
             </li>
