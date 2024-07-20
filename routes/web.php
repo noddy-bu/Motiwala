19cb445ca6ab9b702ccf48a78e9d29fe12c013fe
Faisal <khanfasial.makent@gmail.com>
2024-07-20 17:48:00 +0530

    faisal

--- routes/web.php
+++ routes/web.php
@@ -189,9 +189,9 @@ Route::get('/update-session', function () {
 
 
 Route::get('/custom-session', function () {
-    Session()->put('step', 13);
-    Session()->put('payment', 1);
-    session()->put('temp_user_id', 6);
+    Session()->put('step', 12);
+    // Session()->put('payment', 1);
+    session()->put('temp_user_id', 2);
 });
 
 
