8513bc1bf1e973a967bcc5b135b1e4ad864885d2
Faisal <khanfasial.makent@gmail.com>
2024-07-20 20:01:55 +0530

    fasial

--- resources/views/frontend/pages/online_enrollment/reg_form.blade.php
+++ resources/views/frontend/pages/online_enrollment/reg_form.blade.php
@@ -1793,7 +1793,7 @@
                     var response = xhr.responseText.trim();
                     if (response === 'true') {
                         // Redirect to success page or perform other actions
-                        window.location.href = '{{ url(route('pay-installments')) }}';
+                        window.location.href = "{{ route('pay-installments') }}";
                     } else {
                         window.location.href = "{{ url(route('index')) }}/#sign";
                     }
