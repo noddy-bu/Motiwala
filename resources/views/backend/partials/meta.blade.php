b4a1f9106e3fdee915ab0132c95142f86b1373c1
Faisal <khanfasial.makent@gmail.com>
2024-08-06 15:34:56 +0530

    no index

--- resources/views/backend/partials/meta.blade.php
+++ resources/views/backend/partials/meta.blade.php
@@ -1,6 +1,7 @@
 <base id="baseUrl" href="{{url('')}}">
 <meta charset="utf-8">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">    
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="robots" content="noindex">
 <title>Motiwala Jewels Gold and Diamonds Pvt Ltd</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description">
