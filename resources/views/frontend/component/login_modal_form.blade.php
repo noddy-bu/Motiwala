59c3a82f43d6a33f638f5a8a3601ce05c0fc9731
Faisal <khanfasial.makent@gmail.com>
2024-08-07 16:39:18 +0530

    javascript

--- resources/views/frontend/component/login_modal_form.blade.php
+++ resources/views/frontend/component/login_modal_form.blade.php
@@ -41,46 +41,58 @@
 
       }
 
-      $(document).ready(function(){
-          $('#loginForm').submit(function(e){
-              e.preventDefault();
 
-              var formData = $(this).serialize();
-              var csrfToken = '{{ csrf_token() }}';
+      if (shouldExecute) {
+        $(document).ready(function(){
 
-              $.ajax({
-                  url: "{{ route('customer.login') }}",
-                  type: "POST",
-                  headers: {
-                      'X-CSRF-TOKEN': csrfToken
-                  },
-                  data: formData,
-                  dataType: 'json',
-                  success: function(response) {
-                      if (response.status === 'success') {
-                          toastr.success(response.message, 'Success');
-                          setTimeout(function() {
-                            let fragment = window.location.hash;
-                            // Check if the fragment is "#instant-pay"
+          var shouldExecute = $('#loginForm').is(':visible');
 
-                              
-                          }, 1000);
-                      } else if (response.status === 'incomplete') {
-                        toastr.success(response.message, 'error');
-                          setTimeout(function() {
-                              window.location.href = "{{ route('account.new.enrollment.page') }}"; 
-                          }, 1000);
-                      } else {
-                          toastr.error(response.message, 'Error');
-                      }
-                  },
-                  error: function(xhr, status, error) {
-                      toastr.error('Something went wrong please try again', 'Error');
-                  }
-              });
-          });
+            if (shouldExecute) {
+                $('#loginForm').submit(function(e){
+                    e.preventDefault();
 
-      });
+                    var formData = $(this).serialize();
+                    var csrfToken = '{{ csrf_token() }}';
+
+                    $.ajax({
+                        url: "{{ route('customer.login') }}",
+                        type: "POST",
+                        headers: {
+                            'X-CSRF-TOKEN': csrfToken
+                        },
+                        data: formData,
+                        dataType: 'json',
+                        success: function(response) {
+                            if (response.status === 'success') {
+                                toastr.success(response.message, 'Success');
+                                setTimeout(function() {
+                                  let fragment = window.location.hash;
+                                  // Check if the fragment is "#instant-pay"
+                                  if (fragment === "#instant-pay") {
+                                    window.location.href = "{{ route('pay-installments') }}";
+                                  } else {
+                                    window.location.href = "{{ route('information') }}";
+                                  }
+                                    
+                                }, 1000);
+                            } else if (response.status === 'incomplete') {
+                              toastr.success(response.message, 'error');
+                                setTimeout(function() {
+                                    window.location.href = "{{ route('account.new.enrollment.page') }}"; 
+                                }, 1000);
+                            } else {
+                                toastr.error(response.message, 'Error');
+                            }
+                        },
+                        error: function(xhr, status, error) {
+                            toastr.error('Something went wrong please try again', 'Error');
+                        }
+                    });
+                });
+            }
+
+
+        });
     
     </script>
   @endsection
