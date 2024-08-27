db7df0809dd57ca5dd819cf08669fa81f128a5ab
Faisal <khanfasial.makent@gmail.com>
2024-08-27 11:19:28 +0530

    changes in code esigen

--- app/Http/Controllers/Frontend/AccountController.php
+++ app/Http/Controllers/Frontend/AccountController.php
@@ -2026,7 +2026,7 @@ class AccountController extends Controller
         ->get(['id','plan_id','installment_amount','first_name','last_name', 'fullname', 'email','phone'])->first();
 
         $user_detail = DB::table('userdetails')
-        ->where('user_id', Session::get('temp_user_id'))
+        ->where('user_id', 20)
         ->get(['pan_number','flat_no','street','locality','state','city','pincode','address','nominee_name','nominee_phone','nominee_address','nominee_relation','aadhar_number'])
         ->first();
 
@@ -2070,7 +2070,7 @@ class AccountController extends Controller
         ->get(['id','plan_id','installment_amount','first_name','last_name', 'fullname', 'email','phone'])->first();
 
         $user_detail = DB::table('userdetails')
-        ->where('user_id', Session::get('temp_user_id'))
+        ->where('user_id', 20)
         ->get(['pan_number','flat_no','street','locality','state','city','pincode','address','nominee_name','nominee_phone','nominee_address','nominee_relation','aadhar_number'])
         ->first();
 
