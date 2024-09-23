a159ac530a84b01306ac80194f70da5e9d313b7d
Mehtab645 <mehtab.makent@gmail.com>
2024-09-23 15:00:39 +0530

    banner change, footer  changes

--- resources/views/frontend/pages/admin/new_plan_purchase/index.blade.php
+++ resources/views/frontend/pages/admin/new_plan_purchase/index.blade.php
@@ -17,7 +17,7 @@
     </section>
 
 
-    <main class="main h_100_vh" >
+    <main class="main" >
         <section class="openaccount pt-md-5 pt-3 pb-md-5 pb-5">
             <div class="container">
                 <div class="row">
--- resources/views/frontend/pages/home/index.blade.php
+++ resources/views/frontend/pages/home/index.blade.php
@@ -97,28 +97,28 @@
                 <div class="carousel-item active">
                     <img src="/assets/frontend/images/1.jpg" class="d-md-block d-none w-100"
                         alt="...">
-                    <img src="/assets/frontend/images/5.jpg" class="d-md-none d-block w-100"
+                    <img src="/assets/frontend/images/5-new.jpg" class="d-md-none d-block w-100"
                         alt="...">
                 </div>
 
                 <div class="carousel-item ">
                     <img src="/assets/frontend/images/2.jpg" class="d-md-block d-none w-100"
                         alt="...">
-                    <img src="/assets/frontend/images/6.jpg" class="d-md-none d-block w-100"
+                    <img src="/assets/frontend/images/6-new.jpg" class="d-md-none d-block w-100"
                         alt="...">
                 </div>
 
                 <div class="carousel-item">
                     <img src="/assets/frontend/images/3.jpg" class="d-md-block d-none w-100"
                         alt="...">
-                    <img src="/assets/frontend/images/7.jpg" class="d-md-none d-block w-100"
+                    <img src="/assets/frontend/images/7-new.jpg" class="d-md-none d-block w-100"
                         alt="...">
                 </div>
 
                 <div class="carousel-item">
                     <img src="/assets/frontend/images/4.jpg" class="d-md-block d-none w-100"
                         alt="...">
-                    <img src="/assets/frontend/images/8.jpg" class="d-md-none d-block w-100"
+                    <img src="/assets/frontend/images/8-new.jpg" class="d-md-none d-block w-100"
                         alt="...">
                 </div>
 
