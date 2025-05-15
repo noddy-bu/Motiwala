a955ccf9ec42e1d28f87515dda9b13cbfc890b05
Mehtab645 <mehtab.makent@gmail.com>
2025-05-15 12:10:04 +0530

    chnages done

--- resources/views/frontend/partials/css.blade.php
+++ resources/views/frontend/partials/css.blade.php
@@ -8,12 +8,12 @@
 <link rel="shortcut icon" href="{{ asset('/assets/frontend/images/favicon.png') }}">
 
 <!------------------- motiwala ---------------->
-<link rel="stylesheet" href="/assets/frontend/css/style.css?v=1.0.4" />
+<link rel="stylesheet" href="/assets/frontend/css/style.css?v=1.0.6" />
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet">
 <!-- <link rel="stylesheet" href="/assets/frontend/css/aos.css" /> -->
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
-<link rel="stylesheet" href="/assets/frontend/css/responsive.css" />
+<link rel="stylesheet" href="/assets/frontend/css/responsive.css?v=1.0.5" />
 <link rel="stylesheet" href="/assets/backend/css/custom.css" />
 <link rel="stylesheet" href="/assets/frontend/css/bootstrap.min.css" />
 <link rel="stylesheet" href="/assets/frontend/css/owl.carousel.min.css" />
