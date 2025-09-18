18632eb27c5592171a8d19a9cf805d1970df8ee2
Faisal <khanfasial.makent@gmail.com>
2025-09-18 12:47:52 +0530

    user model update

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    protected $table = 'users'; // lowercase
     protected $fillable = [
         'accept_term',
         'salutation',
